668028a202feace914ee6b2104655af1b79d892e
Sujay <sujayjaiswal03@gmail.com>
2023-11-23 03:59:45 +0530

    minor--bug--fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { Analytics } from '@vercel/analytics/react';
 import { AuthProvider } from './../context/authContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <AuthProvider><Provider store={store}>  <Component {...pageProps} /> <Analytics /></Provider></AuthProvider>
+  return <AuthProvider><Provider store={store}>  <Component {...pageProps} /><ToastContainer /> <Analytics /></Provider></AuthProvider>
 }
